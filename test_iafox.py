2c14359a0a04c48016d49f71b5af16e3be68fde7
webapp_jx <bruno@iafox.com>
2019-03-26 11:12:39 -0300

    fix3

--- test_iafox.py
+++ test_iafox.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 import os
 
 email = "suporte@iafox.com"
