3e3397e8e44391e0bccd8e139d43e2e611a023ed
webapp_jx <bruno@iafox.com>
2019-03-26 10:35:07 -0300

    path to geckodriver

--- test_iafox.py
+++ test_iafox.py
@@ -5,7 +5,7 @@ import os
 email = "suporte@iafox.com"
 senha = "4321"
 
-browser = webdriver.Firefox()
+browser = webdriver.Firefox(executable_path='driver/geckodriver')
 browser.maximize_window()
 browser.get('https://demo1.test.iafox.com/')
 
