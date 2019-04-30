51ffe5b2db804861adec417d1b26463409a46f62
webapp_jx <bruno@iafox.com>
2019-04-30 15:38:31 -0300

    New_tests

--- Test.py
+++ Test.py
@@ -3,18 +3,19 @@ from selenium.webdriver.firefox.options import Options
 import time
 
 def test_setup():
-    time.sleep(120)
-    options = Options()
-    options.headless = True
+    #time.sleep(120)
+    #options = Options()
+    #options.headless = True
     global driver
-    driver = wd.Firefox(options=options)
-    #driver = wd.Firefox()
+    #driver = wd.Firefox(options=options)
+    driver = wd.Firefox()
     driver.get('https://demo1.test.iafox.com')
     time.sleep(2)
 
 def test_login():
     if "Login no IAFOX" == driver.title:
         print("TITULO VALIDO = " + driver.title)
+        driver.save_screenshot('pics/tela_login.png')
     else:
         print("TITULO INVALIDO = " + driver.title)
         driver.close()
@@ -28,8 +29,61 @@ def test_login():
     login = driver.find_element_by_id("ctlForm")
     login.click()
     print("TESTE DE LOGIN OK")
+    time.sleep(1)
+    driver.save_screenshot('pics/tela_inicial.png')
+
+def test_cadastros():
+    cadastro = driver.find_element_by_xpath("//div[contains(text(), 'Cadastros')]")
+    cadastro.click()
+    time.sleep(1)
+    driver.save_screenshot('pics/tela_cadastros.png')
+    
+def test_moldes():
+    driver.get('https://demo1.test.iafox.com/')
+    cadastro = driver.find_element_by_xpath("//div[contains(text(), 'Cadastros')]")
+    cadastro.click()
+    moldes = driver.find_element_by_xpath("//div[@class='mblListItemLabel'][contains(text(),'Molde')]")
+    time.sleep(1)
+    moldes.click()
+    time.sleep(1)
+    print("Moldes OK")
+    driver.save_screenshot('pics/tela_moldes.png')
+
+def test_produtos():
+    driver.get('https://demo1.test.iafox.com/')
+    cadastro = driver.find_element_by_xpath("//div[contains(text(), 'Cadastros')]")
+    cadastro.click()
+    produtos = driver.find_element_by_xpath("//div[contains(text(),'Produto')]")
+    time.sleep(1)
+    produtos.click()
+    print("Produtos OK")
+    time.sleep(1)
+    driver.save_screenshot('pics/tela_produtos.png')
+
+def test_familias():
+    driver.get('https://demo1.test.iafox.com/')
+    cadastro = driver.find_element_by_xpath("//div[contains(text(), 'Cadastros')]")
+    cadastro.click()
+    familias = driver.find_element_by_xpath("//div[contains(text(),'Família')]")
+    time.sleep(1)
+    familias.click()
+    print("Familias OK")
+    time.sleep(1)
+    driver.save_screenshot('pics/tela_familias.png')
+
+def test_etiquetas():
+    driver.get('https://demo1.test.iafox.com/')
+    cadastro = driver.find_element_by_xpath("//div[contains(text(), 'Cadastros')]")
+    cadastro.click()
+    etiquetas = driver.find_element_by_xpath("//div[contains(text(),'Etiquetas')]")
+    time.sleep(1)
+    etiquetas.click()
+    print("Etiquetas OK")
+    time.sleep(1)
+    driver.save_screenshot('pics/tela_etiquetas.png')
 
 def test_logout():
+    driver.get('https://demo1.test.iafox.com/')
     time.sleep(2)
     logout = driver.find_element_by_xpath("//div[contains(text(), 'suporte@iafox.com')]")
     logout.click()
